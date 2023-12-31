60eec0de8fe13eac2cb603ddcb6c5c118d633e1e
Siamak Irani <siamakirani1378@gmail.com>
2023-12-31 22:24:41 +0330

    feat: AnswerForm added

---
+++ src/components/home-page/AnswerForm.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import classes from "./AnswerForm.module.css";
+import CustomBtn from "../CustomBtn";
+
+const AnswerForm = () => {
+  return (
+    <form className={`${classes["answer-form"]}`}>
+      <input type="text" placeholder="پاسخ خود را بنویسید" />
+      <CustomBtn
+        className={classes["btn"]}
+      >
+        دیدن نتیجه
+      </CustomBtn>
+    </form>
+  );
+};
+
+export default AnswerForm;
