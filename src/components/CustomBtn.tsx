a4491ee78b665d18c94a3e4154e30f625b1d8bcd
Siamak Irani <siamakirani1378@gmail.com>
2023-12-31 22:03:38 +0330

    feat: Adding tooltip to CustomBtn

--- src/components/CustomBtn.tsx
+++ src/components/CustomBtn.tsx
@@ -1,16 +1,56 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import classes from "./CustomBtn.module.css";
+import { CustomElementProps } from "../lib/type";
 
-interface CustomBtnProps extends React.ComponentPropsWithoutRef<"button"> {
-  children?: React.ReactNode;
+interface CustomBtnProps extends CustomElementProps<"button"> {
+  tooltipText?: string | null;
+  tooltipClassName?: string | null;
 }
 
-const CustomBtn = ({ children, className, ...rest }: CustomBtnProps) => {
+const CustomBtn = ({
+  children,
+  className,
+  tooltipClassName,
+  tooltipText,
+  ...rest
+}: CustomBtnProps) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [tooltipIsOpen, setTooltipIsOpen] = useState(false);
+  let timeoutId: NodeJS.Timeout;
+
+  const mouseOverHandler = () => {
+    timeoutId = setTimeout(() => {
+      setTooltipIsOpen(true);
+    }, 2 * 1000);
+  };
+
+  const mouseOutHandler = () => {
+    setTooltipIsOpen(false);
+  };
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
   return (
-    <button className={`${classes["CustomBtn"]} ${className}`} {...rest}>
-      {children}
-    </button>
+    <div
+      className={`${classes["CustomBtn"]}`}
+      ref={containerRef}
+      onMouseOver={mouseOverHandler}
+      onMouseOut={mouseOutHandler}
+    >
+      {tooltipText && tooltipIsOpen && (
+        <div className={`${classes["tooltip"]} ${tooltipClassName}`}>
+          {tooltipText}
+        </div>
+      )}
+      <button className={`${classes["btn"]} ${className}`} {...rest}>
+        {children}
+      </button>
+    </div>
   );
 };
 
